0de3b5b5c81c387cf064a5567e692a7d84b4b5c8
CESARIUX2596 <catq2596@gmail.com>
2019-07-11 18:42:35 -0700

    main file

---
+++ escenarinator.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import cv2
+
+
+
+# this program plots types of structures
+
+measure = 1.25
+
+def calculator(side_a, side_b):
+	num_side_a = int(side_a / measure)
+	num_side_b = int(side_b / measure)
+
+	return num_side_a, num_side_b, 
+
+def plotter()
+
+a = 8.75
+b = 3.75
+
+total_a, total_b = calculator(a,b)
+
+print(f"Lado A = {total_a}, Lado B = {total_b}")
+
+side_a = base_ploter(total_a)
+
+for i in range(total_a):
+	if(side_a == True):
+		print ("X")
+	else:
+		print ("NONE")
